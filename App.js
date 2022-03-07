eb600ad7a3dbf9d06d268c927588d2e769ee7436
Marcelo Junior <marcelohjw@gmail.com>
2022-03-07 18:50:42 -0300

    Getting permissions

--- App.js
+++ App.js
@@ -1,8 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
+import React, { useEffect } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import * as Notifications from 'expo-notifications';
+import * as Permissions from 'expo-permissions';
 
 export default function App() {
+
+  useEffect(() => {
+    //Permissions.getAsync(Permissions.NOTIFICATIONS);
+    Notifications.getPermissionsAsync().then(statusObj => {
+      if (statusObj.status !== 'granted') {
+        return Permissions.askAsync(Permissions.NOTIFICATIONS);
+      }
+      return statusObj;
+    }).then(statusObj => {
+      if (statusObj.status !== 'granted') {
+        return;
+      }
+    });
+  }, []);
 
   const triggerNotificationHandler = () => {
     Notifications.scheduleNotificationAsync({
@@ -19,7 +35,10 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Button title='Aperte' onPress={triggerNotificationHandler} />
+      <Button 
+        title='Aperte' 
+        onPress={triggerNotificationHandler} 
+      />
       <Text>Esse é o app de notificação!</Text>
       <StatusBar style="auto" />
     </View>
